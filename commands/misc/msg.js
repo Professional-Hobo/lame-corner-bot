cfc95afa9f70dc485fa8764286bf56bbe83aef97
Farhankhan Daya <lobabob@users.noreply.github.com>
2020-07-06 19:57:06 -0700

    Should be done. Includes full functionality for add/get/set operations with shortform arguments supported.

--- commands/misc/msg.js
+++ commands/misc/msg.js
@@ -1,8 +1,9 @@
 'use strict'
 
 const Command = require('../../lib/Command')
-const { Message, MessageCollector } = require('discord.js')
-const logger = require('../../lib/logger')
+const { MessageCollector, MessageAttachment, MessageEmbed, Channel, Message } = require('discord.js')
+const axios = require('axios')
+const embedRes = require('../../lib/embedRes')
 
 class MsgCommand extends Command {
   constructor(client) {
@@ -17,11 +18,22 @@ class MsgCommand extends Command {
       userPermissions: ['MANAGE_MESSAGES'],
       clientPermissions: ['MANAGE_MESSAGES'],
       guildOnly: true,
-      description: 'Creates/updates/gets message data',
+      description: 'Message data manipulation (content and embeds both). Supports add/get/set operations.',
+      details: 'If using embed info, https://discohook.org/ is very helpful in constructing the relevant JSON. \
+If a message string isn\'t provided, you will be prompted for message data in JSON form. Should look like:\n\n\
+```javascript\n\
+{\n\
+  "content": "message string here",\n\
+  "embeds": []\n\
+}\n\
+```',
       examples: [
-        'msg create #channel-name',
+        'msg add #channel-name',
+        'msg add #channel-name "message string here"',
+        'msg get #channel-name <messageId>',
         'msg get #channel-name 1234343234',
-        'msg update #channel-name 1234343234',
+        'msg set #channel-name 1234343234',
+        'msg set #channel-name 1234343234 "message string here"',
       ],
       argsPromptLimit: 0,
       args: [{
@@ -29,59 +41,159 @@ class MsgCommand extends Command {
         label: 'operation',
         prompt: '',
         type: 'string',
-        oneOf: ['create', 'get', 'update'],
+        oneOf: ['add', 'get', 'set'],
       }, {
         key: 'channel',
         prompt: '',
         type: 'channel',
       }, {
         key: 'messageId',
+        label: 'messageId/content',
         prompt: '',
-        type: 'string', // Can't use type message since message can be from any channel in the guild
+        type: 'string', // Can't use message type since the target message can be from any channel in the guild
+        default: '',
+      }, {
+        key: 'content',
+        label: '',
+        prompt: '',
+        type: 'string',
         default: '',
       }],
     })
   }
 
+  /**
+   * Get a message from a specified channel
+   *
+   * @param {Message} msg Message containing the command
+   * @param {Channel} channel Channel to create the message in
+   * @param {string} messageId Id of the message to retrieve. Must be exist within the provided channel.
+   * @returns {Promise}
+   */
+  async getMsg(msg, channel, messageId) {
+    const srcMsg = await channel.messages.fetch(messageId)
+    const payload = JSON.stringify({ content: srcMsg.content, embeds: srcMsg.embeds }, null, 2)
+
+    if (payload.length > 2000) {
+      return msg.channel.send('Contents were larger than 2000 chars, so it\'s attached as a file instead',
+        new MessageAttachment(Buffer.from(payload), 'content.json'))
+    } else {
+      return msg.say(`\`\`\`\n${payload}\n\`\`\``)
+    }
+  }
+
+  /**
+   * Prompts command author for message data. Must be in the form of JSON.
+   *
+   * @param {Message} msg Message containing the command
+   * @returns {Promise}
+   */
+  async askForMsgData(msg) {
+    await msg.channel.send('Data for the message? Must be valid JSON. Can be an attachment as well.')
+    const collector = new MessageCollector(msg.channel, m => m.author.id === msg.author.id, { max: 1 })
+
+    const data = await new Promise((resolve, reject) => {
+      collector.on('collect', async res => {
+        try {
+          // Check if there's an attachment to use. If so, it will override any provided content.
+          if (res.attachments.size > 0) {
+            const { url } = res.attachments.first()
+            const { data: fileData } = await axios.get(url, { responseType: 'arraybuffer' })
+
+            res.content = Buffer.from(fileData, 'binary').toString()
+          }
+
+          res.content = (res.content.match(/^```(.*?)```$/s) || [])[1] || res.content
+          resolve(JSON.parse(res.content))
+        } catch (err) {
+          reject(err)
+        }
+      })
+    })
+
+    data.embeds = (data.embeds || []).map(embedData => new MessageEmbed(embedData))
+    return data
+  }
+
+  /**
+   * Creates a message in a specified channel.
+   *
+   * @param {Message} msg Message containing the command
+   * @param {Channel} channel Channel to create the message in
+   * @param {string} [content] Shortform. If not included, command author is prompted for message json body.
+   * @returns {Promise}
+   */
+  async addMsg(msg, channel, content) {
+    if (content) {
+      return channel.send(content)
+    }
+
+    const data = await this.askForMsgData(msg)
+    return channel.send(data.content, data.embeds)
+  }
+
+  /**
+   * Sets an existing messages contents in a specified channel.
+   *
+   * @param {Message} msg Message containing the command
+   * @param {Channel} channel Channel the message in
+   * @param {string} messageId Id of the message to set the contents of. Must be exist within the provided channel.
+   * @param {string} [content] Shortform. If not included, command author is prompted for message json body.
+   * @returns {Promise}
+   */
+  async setMsg(msg, channel, messageId, content) {
+    const srcMsg = await channel.messages.fetch(messageId)
+
+    if (content) {
+      return srcMsg.edit(content)
+    }
+
+    const data = await this.askForMsgData(msg)
+
+    await srcMsg.edit(data)
+    for (let i = 0; i < data.embeds.length; ++i) {
+      await srcMsg.edit(data.embeds[i])
+    }
+  }
+
+  /**
+   * Command executor function
+   *
+   * @param {Message} msg Message containing the command
+   * @param {object} args Contains command arguments
+   * @returns {Promise}
+   */
   async run(msg, args) {
-    if (args.op !== 'create' && !args.messageId) {
-      return msg.say(`<@${msg.author.id}>, Invalid command usage. The \`messageId\` is only optional \
-for the \`create\` operation.`)
+    super.run(msg, args)
+
+    if (args.op !== 'add' && !args.messageId) {
+      return msg.say(`<@${msg.author.id}>, Invalid command usage. The \`messageId\` is required \
+for non-\`add\` operations.`)
     }
 
-    const message = args.op === 'create' ?
-      new Message(this.client, {}, args.channel) :
-      await args.channel.messages.fetch(args.messageId)
+    try {
+      switch (args.op) {
+      case 'add':
+        await this.addMsg(msg, args.channel, args.messageId)
+        break
+      case 'get':
+        await this.getMsg(msg, args.channel, args.messageId)
+        break
+      case 'set':
+        await this.setMsg(msg, args.channel, args.messageId, args.content)
+        break
+      }
+    } catch (err) {
+      if (err.name === 'DiscordAPIError' && err.code === 50005) {
+        err.res = embedRes.warn('Do not have permissions to edit!', 'Can only edit messages authored by this bot.')
+      }
 
-    // TODO There's a problem when the response is greater than 2000 characters. Give response in attachment instead?
-    if (args.op === 'get') {
-      // console.log(message)
-      // console.log(JSON.stringify({ content: message.content, embeds: message.embeds }, null, 2))
-      return msg.say(`\`\`\`\n${JSON.stringify({ content: message.content, embeds: message.embeds }, null, 2)}\n\`\`\``)
-    }
+      if (err.name === 'DiscordAPIError' && err.code === 50035) {
+        err.res = embedRes.warn('Message not Found!', `Message id [${msg.id}] does not exist \
+within <#${args.channel.id}>`)
+      }
 
-    // Prompt for message data
-    // await msg.channel.send('Data for the message? Must be a JSON body.')
-    // const collector = new MessageCollector(msg.channel, m => m.author.id === msg.author.id, { time: 30000 })
-
-    // const data = await new Promise((resolve, reject) => {
-    //   collector.on('collect', res => {
-    //     try {
-    //       console.log(res.content)
-    //       resolve(JSON.parse(res.content))
-    //     } catch (err) {
-    //       reject(err)
-    //     }
-    //   })
-    // })
-
-    // await message.edit({
-    //   content: data.content,
-    //   embed: data.embed,
-    // })
-
-    if (args.op === 'create') {
-      args.channel.send(message)
+      throw err
     }
   }
 }
